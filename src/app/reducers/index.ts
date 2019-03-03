6eabbd0c5c4770e246598973740d86599f302992
Rodrigo <rospielberg@gmail.com>
2019-03-03 16:02:48 -0300

    Definindo valor inicial ao objeto auth na store

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -16,13 +16,18 @@ type AuthState = {
 }
 /* Fim Criação dos objetos */
 
+const initialAuthState: AuthState = {
+  loggedIn: false,
+  user: undefined
+}
+
 export interface AppState {
   /* Criação dos objetos a serem mantidos */
   auth: AuthState
   /* Fim Criação dos objetos */
 }
 
-function authReducer(state: AuthState, action): AuthState {
+function authReducer(state: AuthState = initialAuthState, action): AuthState {
   switch (action.type) {
 
     case AuthActionTypes.LoginAction:
