d24dcd95cb49281308e16db7190f87b99d936793
Rodrigo <rospielberg@gmail.com>
2019-03-03 16:23:45 -0300

    Externalizando o primeiro reducer

---
+++ src/app/auth/auth.reducer.ts
@@ -0,0 +1,26 @@
+import { Action } from '@ngrx/store';
+import { User } from '../model/user.model';
+import { AuthActionTypes, AuthActions } from './auth.actions';
+
+
+export interface AuthState {
+  loggedIn: boolean,
+  user: User
+}
+
+export const initialAuthState: AuthState = {
+  loggedIn: false,
+  user: undefined
+};
+
+export function authReducer(state = initialAuthState, action: AuthActions): AuthState {
+  switch (action.type) {
+    case AuthActionTypes.LoginAction:
+      return {
+        loggedIn: true,
+        user: action.payload.user
+      }
+    default:
+      return state;
+  }
+}
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,41 +9,13 @@ import { environment } from '../../environments/environment';
 import { User } from '../model/user.model';
 import { AuthActionTypes } from '../auth/auth.actions';
 
-/* Criação dos objetos a serem mantidos */
-type AuthState = {
-  loggedIn: boolean,
-  user: User
-}
-/* Fim Criação dos objetos */
-
-const initialAuthState: AuthState = {
-  loggedIn: false,
-  user: undefined
-}
-
 export interface AppState {
-  /* Criação dos objetos a serem mantidos */
-  auth: AuthState
-  /* Fim Criação dos objetos */
+
 }
 
-function authReducer(state: AuthState = initialAuthState, action): AuthState {
-  switch (action.type) {
 
-    case AuthActionTypes.LoginAction:
-      return {
-        loggedIn: true,
-        user: action.payload.user
-      }
-
-    default:
-      return state;  
-  }
-}
-
-/* Ações que faram alterações em store */
 export const reducers: ActionReducerMap<AppState> = {
-  auth: authReducer /* Para este objeto faça uso desta função */
+  
 };
 
 
